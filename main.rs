a42cc2bb234fcfe0ddecccffd3d0039f5b4438f1
Daxus91 <oscarmunozcleves@gmail.com>
2023-11-02 15:31:02 -0500

    print odd numbers

--- main.rs
+++ main.rs
@@ -1,9 +1,12 @@
 fn main() {
 
-    let mut v = Vec::new();
+    //let mut v = Vec::new();
 
     for i in 1..11{
-        v.push(i);
+        //v.push(i);
+        if i % 2 != 0 {
+            println!("{}", i);
+        }
     }
 
     //println!("{:?}", v);
