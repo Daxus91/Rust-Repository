a3185891b6558744e9b627e0e98ba2545313753c
Daxus91 <oscarmunozcleves@gmail.com>
2023-11-02 23:43:09 -0500

    print odd number from vector - correction

--- main.rs
+++ main.rs
@@ -3,12 +3,13 @@ fn main() {
     let mut v = Vec::new();
 
     for i in 1..11{
-
-        if i % 2 != 0 {
-            v.push(i);
-        }
+        v.push(i);
     }
-
-    println!("{:?}", v);
     
+    for x in 0..v.len() {
+        if v[x] % 2 != 0 {
+            println!("{:?}", v[x]);  
+        }
+    }  
 }
+
